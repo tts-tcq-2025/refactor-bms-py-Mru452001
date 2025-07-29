3a0c43f87c2d380eaf18788edfd99df373d64760
Mru452001 <muttuh1045@gmail.com>
2025-07-29 10:55:55 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 import sys
 from time import sleep
 
+
 def blink_alert(cycles=6, delay=1):
     for _ in range(cycles):
         print('\r* ', end='')
@@ -10,17 +11,33 @@ def blink_alert(cycles=6, delay=1):
         sys.stdout.flush()
         sleep(delay)
 
+
+def is_temp_ok(temp):
+    return 95 <= temp <= 102
+
+
+def is_pulse_ok(pulse):
+    return 60 <= pulse <= 100
+
+
+def is_spo2_ok(spo2):
+    return spo2 >= 90
+
+
 def vitals_ok(temperature, pulseRate, spo2):
-    if temperature > 102 or temperature < 95:
-        print('Temperature critical!')
+    if not is_temp_ok(temperature):
+        print("Temperature critical!")
         blink_alert()
         return False
-    elif pulseRate < 60 or pulseRate > 100:
-        print('Pulse Rate is out of range!')
+
+    if not is_pulse_ok(pulseRate):
+        print("Pulse Rate is out of range!")
         blink_alert()
         return False
-    elif spo2 < 90:
-        print('Oxygen Saturation out of range!')
+
+    if not is_spo2_ok(spo2):
+        print("Oxygen Saturation out of range!")
         blink_alert()
         return False
+
     return True
