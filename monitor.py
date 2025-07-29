d6c57c7fbaa287e53a4031903108fcdf7b75df0a
Mru452001 <muttuh1045@gmail.com>
2025-07-29 10:58:44 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,43 +1,19 @@
-import sys
-from time import sleep
+from alerts import blink_alert 
 
-
-def blink_alert(cycles=6, delay=1):
-    for _ in range(cycles):
-        print('\r* ', end='')
-        sys.stdout.flush()
-        sleep(delay)
-        print('\r *', end='')
-        sys.stdout.flush()
-        sleep(delay)
-
-
-def is_temp_ok(temp):
-    return 95 <= temp <= 102
-
-
-def is_pulse_ok(pulse):
-    return 60 <= pulse <= 100
-
-
-def is_spo2_ok(spo2):
-    return spo2 >= 90
-
+def is_temp_ok(temp): return 95 <= temp <= 102
+def is_pulse_ok(pulse): return 60 <= pulse <= 100
+def is_spo2_ok(spo2): return spo2 >= 90
 
 def vitals_ok(temperature, pulseRate, spo2):
-    if not is_temp_ok(temperature):
-        print("Temperature critical!")
-        blink_alert()
-        return False
+    checks = [
+        (temperature, is_temp_ok, "Temperature critical!"),
+        (pulseRate, is_pulse_ok, "Pulse Rate is out of range!"),
+        (spo2, is_spo2_ok, "Oxygen Saturation out of range!")
+    ]
 
-    if not is_pulse_ok(pulseRate):
-        print("Pulse Rate is out of range!")
-        blink_alert()
-        return False
-
-    if not is_spo2_ok(spo2):
-        print("Oxygen Saturation out of range!")
-        blink_alert()
-        return False
-
+    for value, check_func, message in checks:
+        if not check_func(value):
+            print(message)
+            blink_alert()
+            return False
     return True
